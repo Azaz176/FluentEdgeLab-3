a386d446b919416ae02ffa9b61d3664d5caff5fd
Azaz <Azaz.Ali@cirruslab.io>
2025-12-29 22:31:56 +0530

    vercel

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "@/react-app/index.css";
 import App from "@/react-app/App.tsx";
 import { ThemeProvider } from "@/react-app/context/ThemeContext";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>
 );
